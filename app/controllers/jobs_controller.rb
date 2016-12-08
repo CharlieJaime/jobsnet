ec6480798c4b9339348c4a2be747d8ea1fedfdf8
Charlie Jaime <baroonie10@gmail.com>
2016-12-07 23:24:07 -0800

    added crud actions to jobs controller *not full rendered _form and new views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,45 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
+
   def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def job_params
+    params_require(:job).permit(:title, :description, :company, :url)
+  end
+
+  def find_job
+    @job = Job.find(params[:id])
   end
 end
